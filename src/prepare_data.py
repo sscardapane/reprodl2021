f35696f210d33e20b3ee2e5dc4b4482d837ae9f0
denni_riccardo <denni@diag.uniroma1.it>
2021-10-04 17:49:50 +0200

    data directory is no more tracked, dvc/initial is dvc tracked

---
+++ src/prepare_data.py
@@ -0,0 +1,68 @@
+import torch, torchaudio
+from torch import nn
+
+from pathlib import Path
+import pandas as pd
+
+import sys
+import yaml
+import pickle
+import os
+
+
+class ESC50Dataset(torch.utils.data.Dataset):
+    # Simple class to load the desired folders inside ESC-50
+
+    def __init__(self, path: Path = Path('data/initial/ESC-50'),
+                 sample_rate: int = 8000,
+                 folds = [1]):
+        # Load CSV & initialize all torchaudio.transforms:
+        # Resample --> MelSpectrogram --> AmplitudeToDB
+        self.path = path
+        self.csv = pd.read_csv(path / Path('meta/esc50.csv'))
+        self.csv = self.csv[self.csv['fold'].isin(folds)]
+        self.resample = torchaudio.transforms.Resample(
+            orig_freq=44100, new_freq=sample_rate
+        )
+        self.melspec = torchaudio.transforms.MelSpectrogram(
+            sample_rate=sample_rate)
+        self.db = torchaudio.transforms.AmplitudeToDB(top_db=80)
+
+
+    def __getitem__(self, index):
+        # Returns (xb, yb) pair, after applying all transformations on the audio file.
+        row = self.csv.iloc[index]
+        wav, _ = torchaudio.load(self.path / 'audio' / row['filename'])
+        label = row['target']
+        xb = self.db(
+            self.melspec(
+                self.resample(wav)
+            )
+        )
+        return xb, label
+
+    def __len__(self):
+        # Returns length
+        return len(self.csv)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.stderr.write("Arguments error. Usage:\n")
+        sys.stderr.write("\tpython prepare_data.py data-file\n")
+        sys.exit(1)
+
+    data_path = Path(sys.argv[1])
+    mode = sys.argv[2]
+    obj_name = mode + ".pickle"
+    output_destination = os.path.join("data","prepared", obj_name)
+    os.makedirs(os.path.join("data", "prepared"), exist_ok=True)
+
+    params = yaml.safe_load(open("params.yaml"))["prepared"]
+    sample_rate = params["sample_rate"]
+    folds = params["folds_"+mode]
+
+    object_data = ESC50Dataset(data_path, sample_rate, folds)
+
+    with open(output_destination, 'wb') as out_dest:
+        pickle.dump(object_data, out_dest)
--- src/train.py
+++ src/train.py
@@ -12,44 +12,15 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import pickle
+import sys
+import os
+import yaml
+from prepare_data import ESC50Dataset
 
-class ESC50Dataset(torch.utils.data.Dataset):
-    # Simple class to load the desired folders inside ESC-50
-    
-    def __init__(self, path: Path = Path('data/ESC-50'), 
-                 sample_rate: int = 8000,
-                 folds = [1]):
-        # Load CSV & initialize all torchaudio.transforms:
-        # Resample --> MelSpectrogram --> AmplitudeToDB
-        self.path = path
-        self.csv = pd.read_csv(path / Path('meta/esc50.csv'))
-        self.csv = self.csv[self.csv['fold'].isin(folds)]
-        self.resample = torchaudio.transforms.Resample(
-            orig_freq=44100, new_freq=sample_rate
-        )
-        self.melspec = torchaudio.transforms.MelSpectrogram(
-            sample_rate=sample_rate)
-        self.db = torchaudio.transforms.AmplitudeToDB(top_db=80)
-        
-        
-    def __getitem__(self, index):
-        # Returns (xb, yb) pair, after applying all transformations on the audio file.
-        row = self.csv.iloc[index]
-        wav, _ = torchaudio.load(self.path / 'audio' / row['filename'])
-        label = row['target']
-        xb = self.db(
-            self.melspec(
-                self.resample(wav)
-            )
-        )
-        return xb, label
-        
-    def __len__(self):
-        # Returns length
-        return len(self.csv)
 
 class AudioNet(pl.LightningModule):
-    
+
     def __init__(self, n_classes = 50, base_filters = 32):
         super().__init__()
         self.conv1 = nn.Conv2d(1, base_filters, 11, padding=5)
@@ -63,7 +34,7 @@ class AudioNet(pl.LightningModule):
         self.bn4 = nn.BatchNorm2d(base_filters * 4)
         self.pool2 = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(base_filters * 4, n_classes)
-        
+
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(self.bn1(x))
@@ -78,7 +49,7 @@ class AudioNet(pl.LightningModule):
         x = F.adaptive_avg_pool2d(x, (1, 1))
         x = self.fc1(x[:, :, 0, 0])
         return x
-    
+
     def training_step(self, batch, batch_idx):
         # Very simple training loop
         x, y = batch
@@ -86,7 +57,7 @@ class AudioNet(pl.LightningModule):
         loss = F.cross_entropy(y_hat, y)
         self.log('train_loss', loss, on_step=True)
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
@@ -94,31 +65,58 @@ class AudioNet(pl.LightningModule):
         acc = functional.accuracy(y_hat, y)
         self.log('val_acc', acc, on_epoch=True, prog_bar=True)
         return acc
-        
+
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
-def train():
+def train(train_data, val_data, test_data, n_classes=20, base_filters=16,\
+        batch_size=8, max_epochs=5):
+    # input: ESC50Dataset objects train_data, val_data, test_data
     # This is the main training function requested by the exercise.
     # We use folds 1,2,3 for training, 4 for validation, 5 for testing.
-    
-    # Load data
-    train_data = ESC50Dataset(folds=[1,2,3])
-    val_data = ESC50Dataset(folds=[4])
-    test_data = ESC50Dataset(folds=[5])
 
     # Wrap data with appropriate data loaders
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=8, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=8)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=8)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
 
     pl.seed_everything(0)
 
     # Initialize the network
-    audionet = AudioNet()
-    trainer = pl.Trainer(gpus=1, max_epochs=25)
+    audionet = AudioNet(n_classes, base_filters)
+    trainer = pl.Trainer(max_epochs=max_epochs)
     trainer.fit(audionet, train_loader, val_loader)
 
 if __name__ == "__main__":
-    train()
+
+    if len(sys.argv) != 2:
+        sys.stderr.write("Arguments error. Usage:\n")
+        sys.stderr.write("\tpython prepare.py data-file\n")
+        sys.exit(1)
+
+    train_data_file = os.path.join(sys.argv[1], "train.pickle")
+    val_data_file = os.path.join(sys.argv[1], "val.pickle")
+    test_data_file = os.path.join(sys.argv[1], "test.pickle")
+
+    with open(train_data_file, 'rb') as train_file:
+        train_data = pickle.load(train_file)
+
+    with open(val_data_file, 'rb') as val_file:
+        val_data = pickle.load(val_file)
+
+    with open(test_data_file, 'rb') as test_file:
+        test_data = pickle.load(test_file)
+
+    params = yaml.safe_load(open("../params.yaml"))
+    params_audio_net = params["audio_net"]
+    params_data_loader = params["data_loader"]
+    params_training = params["training"]
+
+    n_classes = params_audio_net["n_classes"]
+    base_filters = params_audio_net["base_filters"]
+
+    batch_size = params_data_loader["batch_size"]
+    max_epochs = params_training["max_epochs"]
+
+    train(train_data, val_data, test_data, n_classes, base_filters, batch_size, max_epochs)
